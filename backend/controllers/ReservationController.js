d4b688cf0599e57e6aab0d3187d65977b6a8ef7d
saidamoussahif <moussahifsaida@.gmail.com>
2022-11-29 09:31:26 +0100

    last commit

--- backend/controllers/ReservationController.js
+++ backend/controllers/ReservationController.js
@@ -1,8 +1,6 @@
 
 const Reservation = require('../models/ReservationModel');
 const nodemailer = require('nodemailer');
-const Reservation = require('../models/ReservationModel')
-const nodemailer = require('nodemailer');
 const Email = process.env.Email
 const Pass = process.env.PASS
 
@@ -35,28 +33,18 @@ const AddReservation = async (req,res)=>{
     }
 
     const DeleteReservation = async (req,res)=>{
-
-
-
-        const Reserve = await Reservation.findById(req.params.id)
-
-        !Cars? res.status(400) &&  Error('Car Not found'): null;
-        const Reserver = await Reservation.findByIdAndUpdate(req.params.id,req.body,{ new : true})
+        const Reserver = await Reservation.findById(req.params.id)
         res.status(200).json(Reserver)
-    }
-
-
+   
         if(!Reserve){
             res.status(400)
             throw new Error('Reservation Not found')
         }
-        
-     
-    
-        await Reservation.remove()
+            await Reservation.remove()
         res.status(200).json({id : req.params.id})
+         }
     
-    }
+    
 
     
 
@@ -94,26 +82,7 @@ const AddReservation = async (req,res)=>{
    
 
 
-    const mailTransporter = nodemailer.createTransport({
-        service: 'outlook',
-        auth: {
-        email: "saida.youcode123@outlook.com",
-        pass  : "saidayoucodesafi123"
-    }
-    });
-    const mailDetails = { 
-        from: 'saida.youcode123@outlook.com',
-        to  : 'moussahifsaida@outlooky.com',
-        subject: 'Sending confirmation Email',
-        text: 'Booking added successfuly !'
-    }  
-    mailTransporter.sendMail(details, (err) =>{
-      if(err){
-        console.log('it has an error', err);
-      }else{
-        console.log('Email sent successfully');
-      }
-    })
+   
 
 
 
